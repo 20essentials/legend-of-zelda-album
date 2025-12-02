fcfbc683c01048a6ea72bd913f8df4b27c3b0b0a
DominicDev <s0l0estudi4r@gmail.com>
2025-12-02 12:36:02 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LEGEND OF ZELDA ';
+export const nameSongs = [
+	"MAIN THEME", /*1*/
+	"FOREST TEMPLE", /*2*/
+	"WINDMILL HUT", /*3*/
+	"STONE TOWER TEMPLE", /*4*/
+	"LANAYRU SAND SEA", /*5*/
+	"SEALED TEMPLE", /*6*/
+	"SKY KEEP", /*7*/
+	"KOLOKTOS", /*8*/
+	"FOLLOW FI", /*9*/
+	"FI'S THEME", /*10*/
+	"IMPA'S FATE", /*11*/
+	"FI'S FAREWELL", /*12*/
+	"LANAYRU SAND SEA", /*13*/
+	"FI'S GRATITUDE", /*14*/
+	"LEGEND OF SKYLOFT", /*15*/
+	"BALLAD OF THE GODDESS", /*16*/
+	"FILE SELECT", /*17*/
+	"SKYLOFT", /*18*/
+	"CRIMSON LOFTWING", /*19*/
+	"ROMANCE IN THE AIR", /*20*/
+]
